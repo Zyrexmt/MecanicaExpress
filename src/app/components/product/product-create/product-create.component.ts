e3d5239112d7222444e6ba4573fd9934691c4b82
Zyrexmt <suehtamars@gmail.com>
2025-07-16 16:01:06 -0300

    v3.4.1 Create created component

--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
+import { Router } from '@angular/router';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
   styleUrls: ['./product-create.component.css']
 })
-export class ProductCreateComponent {
+export class ProductCreateComponent implements OnInit {
+
+  product: Product = {
+    proNome: '',
+    proDescricao: '',
+    proPrecoCusto: 0,
+    proPrecoVenda: 0,
+    proQuantidadeEstoque: 0,
+    proCategoria: '',
+    proCodigoBarras: '',
+    proMarca: '',
+    proUnidadeMedida: '',
+    proAtivo: 'Ativo',
+    proDataCadastro: new Date().toISOString(),
+    proDataAtualizacao: new Date().toISOString(),
+    proFornecedor: 0
+  }
+
+  constructor(private productService: ProductService,
+              private router: Router) {}
+
+  ngOnInit(): void { }
+
+  createProduct(): void {
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto criado com sucesso!');
+      this.router.navigate(['/products']);
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products']);
+  }
 
 }
