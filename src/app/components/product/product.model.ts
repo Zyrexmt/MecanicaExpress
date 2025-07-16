ae5f69d55c6a3e37b38247a050e0d41267d284d7
Zyrexmt <suehtamars@gmail.com>
2025-07-16 15:53:42 -0300

    v3.4 Product Create Development

--- src/app/components/product/product.model.ts
+++ src/app/components/product/product.model.ts
@@ -1,2 +1,16 @@
 export interface Product {
+    proId?: number;
+  proNome: string;
+  proDescricao: string;
+  proPrecoCusto: number;
+  proPrecoVenda: number;
+  proQuantidadeEstoque: number;
+  proCategoria: string;
+  proCodigoBarras: string;
+  proMarca: string;
+  proUnidadeMedida: string;
+  proAtivo: 'Ativo' | 'Inativo';
+  proDataCadastro: string;         // ISO string format ex: '2025-07-16T15:00:00'
+  proDataAtualizacao: string;     // idem
+  proFornecedor: number;
 }
--- src/app/components/product/product.service.ts
+++ src/app/components/product/product.service.ts
@@ -1,9 +1,41 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http"; 
+import { Injectable } from "@angular/core"; 
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { Observable } from "rxjs"; 
+import { Product } from './product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  baseUrl = "http://localhost:8080/produtos"
+
+  constructor(private snackbar: MatSnackBar, private http: HttpClient) { }
+
+  showMessage(msg: string): void {
+    this.snackbar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: "right",
+      verticalPosition: "top"
+    });
+  }
+
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, product);
+  }
+
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl);
+  }
+
+  update(product: Product): Observable<Product> {
+    const url = `${this.baseUrl}/${product.proId}`;
+    return this.http.put<Product>(url, product);
+  }
+
+  delete(id: number): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.delete<Product>(url);
+  }
 }
