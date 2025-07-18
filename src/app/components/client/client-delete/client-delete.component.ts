9b869c27716e0310387101d8e55df5c53a8a82f2
Zyrexmt <suehtamars@gmail.com>
2025-07-17 22:00:24 -0300

    V3.9.1 Delete Client Development

--- src/app/components/client/client-delete/client-delete.component.ts
+++ src/app/components/client/client-delete/client-delete.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ClientService } from '../client.service';
+import { Client } from '../client.model';
+
 
 @Component({
   selector: 'app-client-delete',
@@ -7,4 +11,28 @@ import { Component } from '@angular/core';
 })
 export class ClientDeleteComponent {
 
+  client!: Client;
+
+  constructor(
+    private clientService: ClientService,
+    private router: Router,
+    private route: ActivatedRoute) { }
+
+    ngOnInit(): void {
+      const cliId = this.route.snapshot.paramMap.get('cliId');
+      this.clientService.readById(cliId!).subscribe(client => {
+        this.client = client;
+      });
+    }
+
+    deleteClient(): void {
+      this.clientService.delete(this.client.cliId!).subscribe(() => {
+        this.clientService.showMessage('Cliente excluído com sucesso!');
+        this.router.navigate(['/clients']);
+      });
+    }
+
+    cancel(): void {
+      this.router.navigate(['/clients']);
+    }
 }
--- src/app/components/client/client.service.ts
+++ src/app/components/client/client.service.ts
@@ -29,13 +29,18 @@ export class ClientService {
       return this.http.get<Client[]>(this.baseUrl);
     }
 
+    readById(id: string): Observable<Client> {
+      const url = `${this.baseUrl}/${id}`;
+      return this.http.get<Client>(url);
+    }
+
     update(client: Client): Observable<Client>{
       const url = `${this.baseUrl}/${client.cliId}`;
       return this.http.put<Client>(url, client);
     }
 
-    delete(id: number): Observable<Client>{
+    delete(id: number): Observable<void>{
       const url = `${this.baseUrl}/${id}`;
-      return this.http.delete<Client>(url);
+      return this.http.delete<void>(url);
     }
 }
